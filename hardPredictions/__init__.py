d0a5bd2daa8278abb022616c44a559e4b6223b6f
maaarangogo <maaarangogo@unal.edu.co>
2018-06-26 11:50:45 -0500

    Update __init__.py

--- hardPredictions/__init__.py
+++ hardPredictions/__init__.py
@@ -2,4 +2,3 @@ from hardPredictions.transformer import *
 from hardPredictions.extras import * 
 from hardPredictions.base_model import *
 from hardPredictions.AR import *
-from hardPredictions.HoltWinters import *

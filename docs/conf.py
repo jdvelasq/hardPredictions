67cfe4a1adc7d21f72361ef4103b0d93b806547b
maaarangogo <maaarangogo@unal.edu.co>
2018-06-26 15:47:50 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration ------------------------------------------------
 
@@ -256,7 +257,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'hardDecisions', 'hardDecisions Documentation',
+    (master_doc, 'hardPredictions', 'hardPredictions Documentation',
      [author], 1)
 ]
 
@@ -271,7 +272,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
     (master_doc, 'hardPredictions', 'hardPredictions Documentation',
-     author, 'hardDecisions', 'A Package for Time Series Prediction using Python',
+     author, 'hardPredictions', 'A Package for Time Series Prediction using Python',
      'Miscellaneous'),
 ]
 

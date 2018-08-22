21c4940a0fa18b6f844f52a5c401f767135515c3
maaarangogo <maaarangogo@unal.edu.co>
2018-08-21 22:05:35 -0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from hardPredictions import *
-from transformer import *
-from extras import * 
-#from hardPredictions.base_model import *
-#from hardPredictions.AR import *
-#from hardPredictions.HoltWinters import *

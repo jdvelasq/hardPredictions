9622d034fa762de55a967e3ac22dc7723fabfe68
maaarangogo <maaarangogo@unal.edu.co>
2018-08-28 21:49:20 -0500

    Add files via upload

--- hardPredictions/__init__.py
+++ hardPredictions/__init__.py
@@ -1,5 +1,5 @@
-from hardPredictions import *
-from transformer import *
+from hardPredictions.hardPredictions import *
+from hardPredictions.transformer import *
 from extras import * 
 #from hardPredictions.base_model import *
 #from hardPredictions.AR import *

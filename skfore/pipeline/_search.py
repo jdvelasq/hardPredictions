7c86ca0d0b69484f60695c72f829399105329931
María Alejandra Arango-González <maaarangogo@unal.edu.co>
2021-02-08 16:15:28 -0500

    Add files via upload

---
+++ skfore/pipeline/_search.py
@@ -0,0 +1,45 @@
+"""
+
+"""
+
+from sklearn.model_selection import ParameterGrid
+import copy
+
+
+class GridSearchCV():
+    
+    def __init__(self, model, parameters, error_function = None):
+        self.initial_model = model
+        self.model = model
+        self.parameters = parameters
+        self.param_grid = ParameterGrid(parameters)
+        
+        self.cv_results_ = list()
+        self.best_score_ = None
+        self.best_params_ = None
+        self.best_model_ = None
+        
+    def __repr__(self):
+        return 'GridSearchCV(model = ' + str(self.initial_model) + ', parameters = ' + str(self.parameters) +')'
+    
+    def fit(self, ts, **fit_params):        
+        
+        for parameters in self.param_grid:
+            model = copy.deepcopy(self.model)
+            model.__init__()
+            model = model.update(parameters)
+            model.fit(ts, **fit_params)
+            i_error = model.calc_error(ts)
+            
+            params = parameters
+            params['score'] = i_error
+            self.cv_results_.append(params)
+            
+            if self.best_score_ == None or i_error < self.best_score_:
+                self.best_score_ = i_error
+                self.best_params_ = parameters
+                self.best_model_ = model
+
+        return self
+
+    

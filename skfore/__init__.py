46f23b5831a55a6d9341a645b8e9df981398e951
María Alejandra Arango-González <maaarangogo@unal.edu.co>
2021-02-08 18:35:50 -0500

    Update __init__.py

--- skfore/__init__.py
+++ skfore/__init__.py
@@ -1,8 +1,7 @@
 from skfore.skfore import *
-from skfore.transformer import *
 from skfore.extras import * 
-from skfore.base_model import *
-from skfore.AR import *
-from skfore.HoltWinters import *
 
+from skfore.models import*
+from skfore.pipeline import *
+from skfore.preprocessing import *
 from skfore.datasets import *

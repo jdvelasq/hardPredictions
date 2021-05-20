892b2827e05eef112287cff3616a0d9da8205f80
María Alejandra Arango-González <maaarangogo@unal.edu.co>
2021-05-20 07:01:58 -0500

    Add files via upload

--- skfore/pipeline/Pipeline.py
+++ skfore/pipeline/Pipeline.py
@@ -8,21 +8,24 @@ import skfore.preprocessing.transformer
 
 class Pipeline():
 
-    def __init__(self, ts, transformation = None, model = None):
+    def __init__(self, ts, transformation = None, model = None, error_function = None):
         self.transformation = transformation
         self.model = model
         self.ts = ts
+        self.error_function = error_function
 
 
     def fit(self):
+        error_list = list()
         for trans_i in self.transformation:
             for model_i in self.model:
                 model = model_i
                 trans = trans_i
                 transformed = trans.fit_transform(self.ts)
                 model_s = model.fit(transformed)
-                error = model.calc_error(transformed)
+                error = model.calc_error(transformed, self.error_function)
                 print(trans)
                 print(model)
                 print(error)
-        return error
+                error_list.append(error)
+        return error_list

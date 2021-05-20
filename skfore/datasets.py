ed5a8ab6f095f3300aa23f86bcb4bc511fde455d
María Alejandra Arango-González <maaarangogo@unal.edu.co>
2021-05-20 07:01:05 -0500

    Add files via upload

--- skfore/datasets.py
+++ skfore/datasets.py
@@ -9,57 +9,58 @@ from os.path import join, dirname
 def load_paper():     
     
     module_path = dirname(__file__) 
-    fdata = pandas.Series.from_csv(join(module_path, "datasets/paper.csv"), index_col = 0, header = 0)
+    fdata = pandas.read_csv(join(module_path, "datasets/paper.csv"), index_col = 0, header = 0).iloc[:,0]
     return fdata
 
 def load_www():     
     
     module_path = dirname(__file__) 
-    fdata = pandas.Series.from_csv(join(module_path, "datasets/WWWusage.csv"), index_col = 0, header = 0)
+    fdata = pandas.read_csv(join(module_path, "datasets/WWWusage.csv"), index_col = 0, header = 0).iloc[:,0]
     return fdata
 
 def load_passengers():     
     
     module_path = dirname(__file__) 
-    fdata = pandas.Series.from_csv(join(module_path, "datasets/passengers.csv"), index_col = 0, header = 0)
+    fdata = pandas.read_csv(join(module_path, "datasets/passengers.csv"), index_col = 0, header = 0).iloc[:,0]
     return fdata
 
 def load_sunspot():
     
     module_path = dirname(__file__) 
-    fdata = pandas.Series.from_csv(join(module_path, "datasets/sunspot.csv"), index_col = 0, header = 0)
+    fdata = pandas.read_csv(join(module_path, "datasets/sunspot.csv"), index_col = 0, header = 0).iloc[:,0]
     return fdata
 
 def load_pollution():
     
     module_path = dirname(__file__) 
-    fdata = pandas.Series.from_csv(join(module_path, "datasets/pollution.csv"), index_col = 0, header = 0)
+    fdata = pandas.read_csv(join(module_path, "datasets/pollution.csv"), index_col = 0, header = 0).iloc[:,0]
     return fdata
 
 def load_lynx():
     
     module_path = dirname(__file__) 
-    fdata = pandas.Series.from_csv(join(module_path, "datasets/lynx.csv"), index_col = 0, header = 0)
+    fdata = pandas.read_csv(join(module_path, "datasets/lynx.csv"), index_col = 0, header = 0).iloc[:,0]
     return fdata
 
 """ Machine Learning Mastery Series"""
 def load_champagne():
     
     module_path = dirname(__file__) 
-    fdata = pandas.Series.from_csv(join(module_path, "datasets/champagne.csv"), index_col = 0, header = 0)
+    fdata = pandas.read_csv(join(module_path, "datasets/champagne.csv"), index_col = 0, header = 0).iloc[:,0]
+    fdata.index = pandas.to_datetime(fdata.index)
     return fdata
 
 """ Colombia Electricity Prices """
 def load_contrato():
     
     module_path = dirname(__file__) 
-    fdata = pandas.Series.from_csv(join(module_path, "datasets/contrato.csv"), index_col = 0, header = 0)
+    fdata = pandas.read_csv(join(module_path, "datasets/contrato.csv"), index_col = 0, header = 0).iloc[:,0]
     return fdata
 
 def load_demanda():
     
     module_path = dirname(__file__) 
-    fdata = pandas.Series.from_csv(join(module_path, "datasets/demanda.csv"), index_col = 0, header = 0)
+    fdata = pandas.read_csv(join(module_path, "datasets/demanda.csv"), index_col = 0, header = 0).iloc[:,0]
     return fdata
 
 
--- skfore/file_reader.py
+++ skfore/file_reader.py
@@ -15,23 +15,25 @@ class file_reader():
         pass
         
     def read_dir(directory = '', extension = '*'):
-        if directory != '':
+        if directory[0:2] != '../' and directory[0:1] != './' and directory[0] != '/':
             add_dir_1 = '../'
-            add_dir_2 = '/'
         else:
             add_dir_1 = ''
+        if directory[-1] != '/':
+            add_dir_2 = '/'
+        else:
             add_dir_2 = ''
         files = glob.glob(add_dir_1 + directory + add_dir_2 + '*.' + extension)
         series = list()
         for i in range(len(files)):
             if extension == 'csv':
-                x_series = pandas.Series.from_csv(files[i], index_col = 0, header = 0)
+                x_series = pandas.read_csv(files[i], index_col = 0, header = 0)
                 series.append(x_series)
             elif extension == 'txt':
-                x_series = pandas.Series.from_csv(files[i], index_col = 0, header = 0)
+                x_series = pandas.read_csv(files[i], index_col = 0, header = 0)
                 series.append(x_series)
             elif extension == '*':
-                x_series = pandas.Series.from_csv(files[i], index_col = 0, header = 0)
+                x_series = pandas.read_csv(files[i], index_col = 0, header = 0)
                 series.append(x_series)
             else:
                 #json

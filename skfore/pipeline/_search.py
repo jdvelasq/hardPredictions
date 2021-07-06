271b93b4d3d5a804d5af2c9a8bd7e6d65d4240a3
María Alejandra Arango-González <maaarangogo@unal.edu.co>
2021-07-06 16:15:14 -0500

    Add files via upload

--- skfore/pipeline/_search.py
+++ skfore/pipeline/_search.py
@@ -22,23 +22,51 @@ class GridSearchCV():
     def __repr__(self):
         return 'GridSearchCV(model = ' + str(self.initial_model) + ', parameters = ' + str(self.parameters) +')'
     
-    def fit(self, ts, **fit_params):        
+    def fit(self, ts, tst = None, **fit_params):
+
+            
+        try:
+            
+            for parameters in self.param_grid:
+                model = copy.deepcopy(self.model)
+                model.__init__()
+                model = model.update(parameters)
+                model.fit(ts, **fit_params)
+                i_error = model.calc_error(tst)
+                
+                params = parameters
+                params['score'] = i_error
+                self.cv_results_.append(params)
+                
+                if self.best_score_ == None or i_error < self.best_score_:
+                    self.best_score_ = i_error
+                    self.best_params_ = parameters
+                    self.best_model_ = model
+                
+                print(i_error)
         
-        for parameters in self.param_grid:
-            model = copy.deepcopy(self.model)
-            model.__init__()
-            model = model.update(parameters)
-            model.fit(ts, **fit_params)
-            i_error = model.calc_error(ts)
+        except:
+             
+            for parameters in self.param_grid:
+                model = copy.deepcopy(self.model)
+                model.__init__()
+                model = model.update(parameters)
+                model.fit(ts, **fit_params)
+                i_error = model.calc_error(ts)
+                
+                params = parameters
+                params['score'] = i_error
+                self.cv_results_.append(params)
+                
+                if self.best_score_ == None or i_error < self.best_score_:
+                    self.best_score_ = i_error
+                    self.best_params_ = parameters
+                    self.best_model_ = model
+                
+                print(i_error)
+                    
             
-            params = parameters
-            params['score'] = i_error
-            self.cv_results_.append(params)
             
-            if self.best_score_ == None or i_error < self.best_score_:
-                self.best_score_ = i_error
-                self.best_params_ = parameters
-                self.best_model_ = model
 
         return self
 
